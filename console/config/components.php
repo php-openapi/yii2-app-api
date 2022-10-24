74d8e64562b2ac0562e43e5383c6bec94bda87cf
Carsten Brandt <mail@cebe.cc>
2022-10-24 15:15:48 +0200

    fix error page, write logs immediately in console

--- console/config/components.php
+++ console/config/components.php
@@ -7,15 +7,18 @@
 return [
     'log' => [
         'traceLevel' => YII_DEBUG ? 3 : 0,
+        'flushInterval' => 1,
         'targets' => [
             [
                 'class' => yii\log\FileTarget::class,
+                'exportInterval' => 1,
                 'levels' => ['error', 'warning'],
                 'logFile' => '@logs/console-app.error.log',
                 'logVars' => [],
             ],
             [
                 'class' => yii\log\FileTarget::class,
+                'exportInterval' => 1,
                 'levels' => ['info'],
                 'logFile' => '@logs/console-app.info.log',
                 'logVars' => [],
